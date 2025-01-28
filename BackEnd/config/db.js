fba550454039271df0906a6e9d556e3092679956
Anish Pande <anishpande1234@gmail.com>
2025-01-28 17:12:09 +0530

    Update db.js

--- BackEnd/config/db.js
+++ BackEnd/config/db.js
@@ -6,7 +6,7 @@ dotenv.config();
 
 const connectDB = async () => {
     try {
-      const uri = 'mongodb+srv://mastersarveshgore:wl44GO95lKN5M5zw@cluster0.xvc04.mongodb.net/test?retryWrites=true&w=majority'; 
+      const uri = process.env.MONGODB_URI; 
       mongoose.connect(uri, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
